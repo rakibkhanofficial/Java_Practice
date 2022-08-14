3476f33cece820b2312bbd668050585851a0fd84
Rakib Khan <39366407+Rakib00111@users.noreply.github.com>
2022-08-14 10:42:42 +0600

    various types of data on java programm

---
+++ datatype/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // character data type
+        /*char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);  */
+        // string data type
+        /* String greeting = "Hello World";
+        System.out.println(greeting); */
+        // boolean datatype
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     // Outputs true
+        System.out.println(isFishTasty);   // Outputs false
+
+
+    }
+}
