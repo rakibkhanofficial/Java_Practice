2111e1094a9e6bcf8763109281341b3fa130871c
Rakib Khan <39366407+Rakib00111@users.noreply.github.com>
2022-08-14 10:41:49 +0600

    Add files via upload
    uses of println function on java programm

---
+++ print/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here println function
+       /* String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName); */
+
+        /*int x = 5;
+        int y = 6;
+        System.out.println(x + y); // Print the value of x + y */
+        // declare multiple variable
+        /*int x = 5;
+        int y = 6;
+        int z = 50;
+        System.out.println(x + y + z); */
+        //can simply write:
+        int x = 5, y = 6, z = 50;
+        System.out.println(x + y + z);
+    }
+}
